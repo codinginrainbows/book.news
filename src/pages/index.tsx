12ebc367d18adbdfadf6e5f29d64376dca571e18
codinginrainbows <gabrielmgarcia96@gmail.com>
2022-01-10 13:45:04 -0300

    first configs

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>News App</title>
+      </Head>
+      <h1>
+        hello friend
+      </h1>
+    </>
+  )
+}
