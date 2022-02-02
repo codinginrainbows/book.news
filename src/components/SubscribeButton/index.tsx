39ca7534c48870205f84264c0259bcb22d6ee87e
Gabriel Garcia <gabrielmgarcia96@gmail.com>
2022-02-02 19:35:56 -0300

    double users in database avoided

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,7 @@
+import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/axios';
+import { getStripeJs } from '../../services/stripe-js';
+
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,11 +9,33 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const session = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId: sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+
+  }
 
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,72 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from 'next-auth/react';
+import { faunadb } from "../../services/fauna";
+import { query as q } from "faunadb";
+import { stripe } from "../../services/stripe";
+
+type User = {
+  ref: {
+    id: string;
+  }
+
+  data: {
+    stripe_customer_id: string;
+  };
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const session = await getSession({ req });
+
+    // getting user from database
+    const user = await faunadb.query<User>(
+      q.Get(
+        q.Match(
+          q.Index('user_by_email'),
+          q.Casefold(session.user.email)
+        )
+      )
+    )
+
+    let customerId = user.data.stripe_customer_id;
+
+    // prevent database from creating 2 users with the same email
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+        //metadata
+      });
+
+      await faunadb.query(
+        q.Update(
+          q.Ref(q.Collection('users'), user.ref.id),
+          {
+            data: {
+              stripe_customer_id: stripeCustomer.id,
+            }
+          }
+        )
+      )
+
+      customerId = stripeCustomer.id;
+    }
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: customerId,
+      payment_method_types: ['card'],
+      billing_address_collection: 'required',
+      line_items: [
+        { price: 'price_1KHA6SJwx3rvawH7cYrondGR', quantity: 1 }
+      ],
+      mode: 'subscription',
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCCESS_URL, //posts route to be created!
+      cancel_url: process.env.STRIPE_CANCEL_URL,
+    });
+
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id });
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method is not allowd. Try method POST, instead.');
+  }
+}
